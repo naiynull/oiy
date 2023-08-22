85af2b43b24617c60c31c415494c30f357021476
naiynull <manuborges011@GMAIL.COM>
2023-08-22 15:47:18 -0300

    l

--- src/pages/Sinopse/index.tsx
+++ src/pages/Sinopse/index.tsx
@@ -1,6 +1,19 @@
 import * as S from "./styles"
 import teste from "../../assets/fundo01.jpg"
 import fundo from "../../assets/fundo.gif"
+{/*imagens do zelda 01*/}
+import zelda11 from "../../assets/zelamarro.avif"
+import zelda12 from "../../assets/zeldeacenasjogo1.jpg"
+import zelda13 from "../../assets/zeldaparte1.avif"
+import zelda14 from "../../assets/zeldacasando.webp"
+import zeldam from "../../assets/wallpaperzelda01.jpg"
+{/*imagens do zelda 02*/}
+import zelda21 from "../../assets/zelda_a2.avif"
+import zelda22 from "../../assets/zelda_b2.jpg"
+import zelda23 from "../../assets/zelda_c2.jpg"
+import zelda24 from "../../assets/zelda_d2.webp"
+import zeldam2 from "../../assets/wallpaperzelda2.jpg"
+
 import { ComponentHeader } from "../../components"
 
 export function Sinopse() {
@@ -16,41 +29,41 @@ export function Sinopse() {
 
             <S.textot>
 
-
+{/*primeiro jogo do zelda*/}
 
                 <div className="main">
 
-                    <h1>MYLOGO.COM</h1>
+                    <h1>The Legend of Zelda a Link to the Past</h1>
 
 
-                    <h2>PORTFOLIO</h2>
+                    <h2>Nitendo</h2>
                     <p>Resize the browser window to see the responsive effect.</p>
 
                     <div className="row">
                         <div className="column">
                             <div className="content">
-                                <img src={teste} alt="Mountains" width='100%' />
+                                <img src={zelda11} alt="Mountains" width='100%' />
                                 <h3>My Work</h3>
                                 <p>Lorem ipsum dolor sit amet, tempor prodesset eos no. Temporibus necessitatibus sea ei, at tantas oporteat nam. Lorem ipsum dolor sit amet, tempor prodesset eos no.</p>
                             </div>
                         </div>
                         <div className="column">
                             <div className="content">
-                                <img src={teste} alt="Lights" width='100%' />
+                                <img src={zelda12} alt="Lights" width='100%' />
                                 <h3>My Work</h3>
                                 <p>Lorem ipsum dolor sit amet, tempor prodesset eos no. Temporibus necessitatibus sea ei, at tantas oporteat nam. Lorem ipsum dolor sit amet, tempor prodesset eos no.</p>
                             </div>
                         </div>
                         <div className="column">
                             <div className="content">
-                                <img src={teste} alt="Nature" width='100%' />
+                                <img src={zelda13} alt="Nature" width='100%' />
                                 <h3>My Work</h3>
                                 <p>Lorem ipsum dolor sit amet, tempor prodesset eos no. Temporibus necessitatibus sea ei, at tantas oporteat nam. Lorem ipsum dolor sit amet, tempor prodesset eos no.</p>
                             </div>
                         </div>
                         <div className="column">
                             <div className="content">
-                                <img src={teste} alt="Mountains" width='100%' />
+                                <img src={zelda14} alt="Mountains" width='100%' />
                                 <h3>My Work</h3>
                                 <p>Lorem ipsum dolor sit amet, tempor prodesset eos no. Temporibus necessitatibus sea ei, at tantas oporteat nam. Lorem ipsum dolor sit amet, tempor prodesset eos no.</p>
                             </div>
@@ -59,7 +72,7 @@ export function Sinopse() {
                     </div>
 
                     <div className="content">
-                        <img src={teste} alt="Bear" width='100%' />
+                        <img src={zeldam} alt="Bear" width='100%' />
                         <h3>Some Other Work</h3>
                         <p>Lorem ipsum dolor sit amet, tempor prodesset eos no. Temporibus necessitatibus sea ei, at tantas oporteat nam. Lorem ipsum dolor sit amet, tempor prodesset eos no.</p>
                         <p>Lorem ipsum dolor sit amet, tempor prodesset eos no. Temporibus necessitatibus sea ei, at tantas oporteat nam. Lorem ipsum dolor sit amet, tempor prodesset eos no.</p>
@@ -69,37 +82,37 @@ export function Sinopse() {
 
                 <div className="main">
 
-                    <h1>MYLOGO.COM</h1>
+                    <h1>The Legend of Zelda: Link's Awakening</h1>
 
 
-                    <h2>PORTFOLIO</h2>
+                    <h2>1993</h2>
                     <p>Resize the browser window to see the responsive effect.</p>
 
                     <div className="row">
                         <div className="column">
                             <div className="content">
-                                <img src={teste} alt="Mountains" width='100%' />
+                                <img src={zelda21} alt="Mountains" width='100%' />
                                 <h3>My Work</h3>
                                 <p>Lorem ipsum dolor sit amet, tempor prodesset eos no. Temporibus necessitatibus sea ei, at tantas oporteat nam. Lorem ipsum dolor sit amet, tempor prodesset eos no.</p>
                             </div>
                         </div>
                         <div className="column">
                             <div className="content">
-                                <img src={teste} alt="Lights" width='100%' />
+                                <img src={zelda22} alt="Lights" width='100%' />
                                 <h3>My Work</h3>
                                 <p>Lorem ipsum dolor sit amet, tempor prodesset eos no. Temporibus necessitatibus sea ei, at tantas oporteat nam. Lorem ipsum dolor sit amet, tempor prodesset eos no.</p>
                             </div>
                         </div>
                         <div className="column">
                             <div className="content">
-                                <img src={teste} alt="Nature" width='100%' />
+                                <img src={zelda23} alt="Nature" width='100%' />
                                 <h3>My Work</h3>
                                 <p>Lorem ipsum dolor sit amet, tempor prodesset eos no. Temporibus necessitatibus sea ei, at tantas oporteat nam. Lorem ipsum dolor sit amet, tempor prodesset eos no.</p>
                             </div>
                         </div>
                         <div className="column">
                             <div className="content">
-                                <img src={teste} alt="Mountains" width='100%' />
+                                <img src={zelda24} alt="Mountains" width='100%' />
                                 <h3>My Work</h3>
                                 <p>Lorem ipsum dolor sit amet, tempor prodesset eos no. Temporibus necessitatibus sea ei, at tantas oporteat nam. Lorem ipsum dolor sit amet, tempor prodesset eos no.</p>
                             </div>
@@ -108,7 +121,7 @@ export function Sinopse() {
                     </div>
 
                     <div className="content">
-                        <img src={teste} alt="Bear" width='100%' />
+                        <img src={zeldam2} alt="Bear" width='100%' />
                         <h3>Some Other Work</h3>
                         <p>Lorem ipsum dolor sit amet, tempor prodesset eos no. Temporibus necessitatibus sea ei, at tantas oporteat nam. Lorem ipsum dolor sit amet, tempor prodesset eos no.</p>
                         <p>Lorem ipsum dolor sit amet, tempor prodesset eos no. Temporibus necessitatibus sea ei, at tantas oporteat nam. Lorem ipsum dolor sit amet, tempor prodesset eos no.</p>
