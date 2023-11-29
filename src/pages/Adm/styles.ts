b04af15f8f50d0dc1f830aca18c682b59ad96320
naiynull <manuborges011@gmail.com>
2023-11-29 14:06:15 -0300

    pronto

--- src/pages/Adm/styles.ts
+++ src/pages/Adm/styles.ts
@@ -2,13 +2,13 @@ import styled from "styled-components"
 
 export const Section = styled.section`
 h1{
-  font-size: 7rem;
+  font-size: 4rem;
   color: #fff;
   display: flex;
   text-align: center;
   text-decoration: none;
-  padding-left: 30rem;
-  padding-top:28rem;
+  padding-left: 4rem;
+  padding-top:2rem;
   font-family: 'The Wild Breath of Zelda', sans-serif;
 }
 `
